ea47c2e39787746c17581994adba845d98fc37b4
JohnnieHuynh <huy.j.huynh@gmail.com>
2022-08-15 10:45:19 -0700

    initial commit

---
+++ MoonInfo/MoonInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonInfo
+//
+//  Created by Johnny Huynh on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonInfo/MoonInfo/MoonInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonInfoApp.swift
+//  MoonInfo
+//
+//  Created by Johnny Huynh on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
